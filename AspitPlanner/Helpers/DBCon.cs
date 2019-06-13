36815079b408d7b9b4e35b368da052117f3e730f
Oliver Stenkilde <oliv6101@skivecollege.dk>
2019-06-13 12:56:35 +0200

    sql ligene ting

---
+++ AspitPlanner/Helpers/DBCon.cs
@@ -0,0 +1,40 @@
+﻿using AspitPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspitPlanner.Helpers
+{
+    public class DBCon : DbContext
+    {
+        public static readonly string con = "Local";
+
+        public DBCon() : base(ConfigurationManager.ConnectionStrings[con].ConnectionString) { }
+        
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Present> Presents { get; set; }
+        public DbSet<Models.Type> Types { get; set; }
+
+               
+        public List<AbsentType> GetAbcentTypes() 
+        {
+            List<AbsentType> retur = new List<AbsentType>();
+            using (DBCon db = new DBCon())
+            {
+                var Abcent = (from a in db.Types
+                              join c in db.Categorys on a.CatID equals c.ID
+                              select new AbsentType { ID = a.ID, TypeName = a.TypeName, CatName = c.CategoryName })
+                               .ToList();
+                retur = Abcent;
+            }
+            return retur;
+
+        }
+    }    
+}
