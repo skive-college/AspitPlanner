23460a86971f36d945b0e397c1c67cd6cbbf8312
Kjeld Flindt Ejlerskov <kjel0737@skivecollege.dk>
2019-06-26 10:42:15 +0200

    Mail Sender klasse

---
+++ AspitPlanner/Helpers/MailSender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspitPlanner.Helpers
+{
+    public class MailSender
+    {
+        public static void SendMail(string msg)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("jenslyn608@gmail.com");
+                mail.To.Add("ke@skivecollege.dk");
+                mail.Subject = "Test Mail";
+                mail.Body = msg;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("Jenslyn608@gmail.com", "aspitmidtjylland");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                
+            }
+            catch (Exception ex)
+            {
+                FileHandler.Error(ex);
+            }
+        }
+
+    }
+}
